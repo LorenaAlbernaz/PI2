516d319dda4cce94dbfc011273e904c80d865485
Lorena Albernaz <30875718+LorenaAlbernaz@users.noreply.github.com>
2019-06-03 14:28:17 -0300

    Update nivel.ino

--- nivel.ino
+++ nivel.ino
@@ -1,27 +1,29 @@
-/**
- * Authorization.ino
- *
- *  Created on: 09.12.2015
- *
- */
-
-#include <Arduino.h>
-
 // Pino ligado ao sensor de nivel de liquido
-int pinosensor = 4;
+int nivel_1 = 4;
+int nivel_2 = 22;
+int nivel_3 = 23;
 
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(pinosensor, INPUT);
+  pinMode(nivel_1 , INPUT);
+  pinMode(nivel_2 , INPUT);
+  pinMode(nivel_3 , INPUT);
 }
 
 void loop()
 {
-  int estado = digitalRead(pinosensor);
-  Serial.print("Estado sensor : ");
-  Serial.println(estado);
+  int estado1 = digitalRead(nivel_1);
+  Serial.print("Nivel 1 : ");
+  Serial.println(estado1);
 
-  delay(100);
+  int estado2 = digitalRead(nivel_2);
+  Serial.print("Nivel 2 : ");
+  Serial.println(estado2);
+
+  int estado3 = digitalRead(nivel_3);
+  Serial.print("Nivel 3: ");
+  Serial.println(estado3);
+  delay(1000);
 }
